9173238c323c32c369e57b1c988238a0357f0dd4
jasontran0 <thuantran433154@gmail.com>
2025-02-12 02:03:41 -0600

    Modified getAllPrices to announce the message in the channel

--- tcgSlackBot.py
+++ tcgSlackBot.py
@@ -24,20 +24,17 @@ BOT_ID = client.api_call("auth.test")['user_id']
 @app.route('/getAllPrices', methods=['POST'])
 def get_all_prices():
     user_id = request.form.get('user_id')
+    channel_id = request.form.get('channel_id')
 
     # Step 1: Open a DM with the user to get a valid channel_id
     dm_response = client.conversations_open(users=[user_id])
     dm_channel_id = dm_response["channel"]["id"]  # Get the correct DM channel ID
-
-    
-    # Run tcgPriceTracker.py script
-    # os.system('python /Users/kyzubs/tcgPriceTracker/tcgPriceTracker.py')
     
     # Step 2: Notify user that they will receive the CSV file
-    client.chat_postMessage(channel=user_id, text='Generating the price list. You will receive the CSV file shortly.')
+    client.chat_postMessage(channel=channel_id, text='Generating the price list. You will be DM\'d the CSV file shortly.')
     
     # Step 3: Queue the background job
-    run_tcg_tracker_and_send.delay(user_id, dm_channel_id)
+    run_tcg_tracker_and_send.delay(dm_channel_id)
 
     return Response(), 200
 
