f66b469afc5f9f90197e9063c0651709b1c4e706
jasontran0 <thuantran433154@gmail.com>
2025-02-12 01:48:59 -0600

    Implemented slash commands for getAllPrices and getPrice(productID); Added functionality for queuing background jobs

--- tcgSlackBot.py
+++ tcgSlackBot.py
@@ -1,27 +1,22 @@
 import slack
 import os
-import requests
 from slack_sdk import WebClient
 from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
-from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
+from celery import Celery
+from tasks import run_tcg_tracker_and_send, get_price_task
 
-# Initializes your app with your bot token and socket mode handler
-# app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
-
-# # Start your app
-# if __name__ == "__main__":
-#     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-
+# load environment variables
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+# flask app setup
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
 
+# slack app setup
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
@@ -32,28 +27,33 @@ def get_all_prices():
 
     # Step 1: Open a DM with the user to get a valid channel_id
     dm_response = client.conversations_open(users=[user_id])
-    channel_id = dm_response["channel"]["id"]  # Get the correct DM channel ID
+    dm_channel_id = dm_response["channel"]["id"]  # Get the correct DM channel ID
 
     
     # Run tcgPriceTracker.py script
     # os.system('python /Users/kyzubs/tcgPriceTracker/tcgPriceTracker.py')
     
-    # Notify user that they will receive the CSV file
+    # Step 2: Notify user that they will receive the CSV file
     client.chat_postMessage(channel=user_id, text='Generating the price list. You will receive the CSV file shortly.')
     
-    # Assuming the script generates a file named 'tcg_price_data.csv' in the same directory
-    file_path = '/Users/kyzubs/tcgPriceTracker/tcg_price_data.csv'
+    # Step 3: Queue the background job
+    run_tcg_tracker_and_send.delay(user_id, dm_channel_id)
 
-    # Upload the csv file to the user's DM
-    response = client.files_upload_v2(
-        channels=channel_id,
-        file=file_path,
-        title='TCG Prices',
-        initial_comment='Here is the latest price list.'
-    )
-    response.get("file")  # returns the full metadata of the uploaded file
     return Response(), 200
 
+@app.route('/getPrice', methods=['POST'])
+def get_price():
+    product_id = request.form.get('text')
+    channel_id = request.form.get('channel_id')
+    print(request.form)
+
+    # Step 1: Notify user that they will receive the CSV file
+    client.chat_postMessage(channel=channel_id, text=f'Generating the price for product ID {product_id}. You will receive the product details shortly.')
+
+    # Step 2: Queue the background job
+    get_price_task.delay(product_id, channel_id)
+
+    return Response(), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
